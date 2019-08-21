ecc8e5915d74c25bd4a196b6897d34980916f833
Your Name <itzalbert2@gmail.com>
2019-08-21 14:56:14 -0700

    Rename and reformat vehicleInfo files

--- __tests__/GMvehicleInfo.test.js
+++ __tests__/GMvehicleInfo.test.js
@@ -1,30 +1,53 @@
-const postMock = require('../mocks/POST/vehicleInfo/postMock');
+const postMock = require('../__mocks__/POST/vehicleInfo/vehicleInfoMock');
+const invalidMock = require('../__mocks__/utils/globalMocks');
 
-describe('POST Request to GM endpoint', () => {
-  let vehicle1234 = null;
-  let vehicle1235 = null;
+describe('POST Request to GM for general Vehicle information', () => {
+  let sedan = null;
+  let twoDoorCoupe = null;
   let invalidVehicle1236 = null;
   let invalidVehicle1999 = null;
 
+  //TODO: Check if throwing an error returns out
   beforeAll(() => {
-    postMock.getVehicleInfo1234().then(res => {
-      vehicle1234 = res;
-    });
-    postMock.getVehicleInfo1235().then(res => {
-      vehicle1235 = res;
-    });
-    postMock.getInvalidVehicle(1236).then(res => {
-      invalidVehicle1236 = res;
-    });
-    postMock.getInvalidVehicle(1999).then(res => {
-      invalidVehicle1999 = res;
-    });
+    postMock
+      .getVehicleInfo1234()
+      .then(res => {
+        sedan = res;
+      })
+      .catch(err => {
+        throw new Error('Unable to receive Vehicle info 1234 in Promise');
+      });
+    postMock
+      .getVehicleInfo1235()
+      .then(res => {
+        twoDoorCoupe = res;
+      })
+      .catch(err => {
+        throw new Error('Unable to receive Vehicle info 1235 in Promise');
+      });
+
+    invalidMock
+      .getInvalidVehicle(1236)
+      .then(res => {
+        invalidVehicle1236 = res;
+      })
+      .catch(err => {
+        throw new Error('Unable to receive Invalid car JSON in Promise');
+      });
+    invalidMock
+      .getInvalidVehicle(1999)
+      .then(res => {
+        invalidVehicle1999 = res;
+      })
+      .catch(err => {
+        throw new Error('Unable to receive Invalid vehicle JSON in Promise');
+      });
   });
 
   test('Should only call the mock function once per available vehicle', () => {
     expect(postMock.getVehicleInfo1234.mock.calls.length).toBe(1);
     expect(postMock.getVehicleInfo1235.mock.calls.length).toBe(1);
-    expect(postMock.getInvalidVehicle.mock.calls.length).toBe(2);
+    expect(invalidMock.getInvalidVehicle.mock.calls.length).toBe(2);
   });
 
   test('Should return a 404 error if a vehicle ID does not exist', () => {
@@ -33,15 +56,15 @@ describe('POST Request to GM endpoint', () => {
   });
 
   test('Should return a status code 200 on success for vehicle 1234', () => {
-    expect(vehicle1234.data.status).toBe('200');
+    expect(sedan.data.status).toBe('200');
   });
 
   test('Should return a status code 200 on success for vehicle 1235', async () => {
-    expect(vehicle1235.data.status).toBe('200');
+    expect(twoDoorCoupe.data.status).toBe('200');
   });
 
   test('GM response should contain a service, status, and data key for vehicle 1234', () => {
-    let objectKeys = new Set(Object.keys(vehicle1234.data)); //[service, status, data];
+    let objectKeys = new Set(Object.keys(sedan.data)); //[service, status, data];
     expect(objectKeys.size).toBe(3);
     expect(objectKeys.has('service')).toBe(true);
     expect(objectKeys.has('status')).toBe(true);
@@ -49,16 +72,15 @@ describe('POST Request to GM endpoint', () => {
   });
 
   test('GM response should contain a service, status, and data key for vehicle 1235', () => {
-    let objectKeys = new Set(Object.keys(vehicle1235.data)); //[service, status, data];
+    let objectKeys = new Set(Object.keys(twoDoorCoupe.data)); //[service, status, data];
     expect(objectKeys.size).toBe(3);
     expect(objectKeys.has('service')).toBe(true);
     expect(objectKeys.has('status')).toBe(true);
     expect(objectKeys.has('data')).toBe(true);
   });
 
-  //Should have vin, color, fourDoorSedan, twoDoorCoupe
   test('GM response data should have a vin, color, fourDoorSedan and twoDoorCoupe keys for vehicle 1234', () => {
-    let objectKeys = new Set(Object.keys(vehicle1234.data.data));
+    let objectKeys = new Set(Object.keys(sedan.data.data));
     expect(objectKeys.size).toBe(5);
     expect(objectKeys.has('vin')).toBe(true);
     expect(objectKeys.has('color')).toBe(true);
@@ -68,7 +90,7 @@ describe('POST Request to GM endpoint', () => {
   });
 
   test('GM response data should have a vin, color, fourDoorSedan and twoDoorCoupe keys for vehicle 1235', () => {
-    let objectKeys = new Set(Object.keys(vehicle1235.data.data));
+    let objectKeys = new Set(Object.keys(twoDoorCoupe.data.data));
     expect(objectKeys.size).toBe(5);
     expect(objectKeys.has('vin')).toBe(true);
     expect(objectKeys.has('color')).toBe(true);
@@ -78,19 +100,24 @@ describe('POST Request to GM endpoint', () => {
   });
 
   test('Vehicle data must have a String type for Vin', () => {
-    expect(vehicle1234.data.data.vin.type).toEqual('String');
+    expect(sedan.data.data.vin.type).toEqual('String');
+    expect(twoDoorCoupe.data.data.vin.type).toEqual('String');
   });
 
   test('Vehicle data must have a String type for color', () => {
-    expect(vehicle1234.data.data.color.type).toEqual('String');
+    expect(sedan.data.data.color.type).toEqual('String');
+    expect(twoDoorCoupe.data.data.color.type).toEqual('String');
   });
 
   test('Vehicle data must have a String type for driveTrain', () => {
-    expect(vehicle1234.data.data.driveTrain.type).toEqual('String');
+    expect(sedan.data.data.driveTrain.type).toEqual('String');
+    expect(twoDoorCoupe.data.data.driveTrain.type).toEqual('String');
   });
 
   test('Vehicle data must have a Boolean type for fourDoorSedan and twoDoorCoupe', () => {
-    expect(vehicle1234.data.data.fourDoorSedan.type).toEqual('Boolean');
-    expect(vehicle1234.data.data.twoDoorCoupe.type).toEqual('Boolean');
+    expect(sedan.data.data.fourDoorSedan.type).toEqual('Boolean');
+    expect(sedan.data.data.twoDoorCoupe.type).toEqual('Boolean');
+    expect(twoDoorCoupe.data.data.fourDoorSedan.type).toEqual('Boolean');
+    expect(twoDoorCoupe.data.data.twoDoorCoupe.type).toEqual('Boolean');
   });
 });
