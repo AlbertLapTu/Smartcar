1c964125051b89d0f51b59f5feefbed4154f94a4
Your Name <itzalbert2@gmail.com>
2019-08-22 10:36:29 -0700

    Adjust absolute path

--- __mocks__/POST/fuel-Battery/fuelBatteryMocks.js
+++ __mocks__/POST/fuel-Battery/fuelBatteryMocks.js
@@ -4,12 +4,14 @@ const coupe = require('./vehicle1235.coupe-battery.fuel.response.json');
 module.exports = {
   getCoupeInfo: jest.fn(() =>
     Promise.resolve({
+      status: coupe.status,
       batteryData: coupe.data.batteryLevel,
       fuelData: coupe.data.fuelLevel
     })
   ),
   getSedanInfo: jest.fn(() =>
     Promise.resolve({
+      status: coupe.status,
       batteryData: sedan.data.batteryLevel,
       fuelData: sedan.data.fuelLevel
     })
