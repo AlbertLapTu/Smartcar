5b9180ad373cc8258bbe48cb156b346a739c638d
Your Name <itzalbert2@gmail.com>
2019-08-22 10:35:00 -0700

    Refactor out GM request payloads to separate files

--- server/api/vehicles/requestOptions.js
+++ server/api/vehicles/requestOptions.js
@@ -1,65 +1,64 @@
-//Need to add in parameters to pass into ID
-
-const getVehicleInfo = id => {
-  return {
-    method: 'POST',
-    url: 'http://gmapi.azurewebsites.net/getVehicleInfoService/',
-    headers: {
-      'content-type': 'application/json'
-    },
-    data: {
-      id: id,
-      responseType: 'JSON'
-    }
-  };
-};
-
-const getSecurityStatus = id => {
-  return {
-    method: 'POST',
-    url: 'http://gmapi.azurewebsites.net/getSecurityStatusService',
-    headers: {
-      'content-type': 'application/json'
-    },
-    data: {
-      id: id,
-      responseType: 'JSON'
-    }
-  };
-};
-
-const getEnergy = id => {
-  return {
-    method: 'POST',
-    url: 'http://gmapi.azurewebsites.net/getEnergyService',
-    headers: {
-      'content-type': 'application/json'
-    },
-    data: {
-      id: id,
-      responseType: 'JSON'
-    }
-  };
-};
-
-const actionEngine = (id, command) => {
-  return {
-    method: 'POST',
-    url: 'http://gmapi.azurewebsites.net/actionEngineService',
-    headers: {
-      'content-type': 'application/json'
-    },
-    data: {
-      id: id,
-      command: command,
-      responseType: 'JSON'
-    }
-  };
-};
+const { formatEngineActionPayload } = require('../../lib/payloadFormattingHelpers');
 
 module.exports = {
-  getVehicleInfo,
-  getSecurityStatus,
-  getEnergy,
-  actionEngine
+  getVehicleInfoService: vehicleId => {
+    let options = {
+      method: 'POST',
+      url: 'http://gmapi.azurewebsites.net/getVehicleInfoService/',
+      headers: {
+        'content-type': 'application/json'
+      },
+      data: {
+        id: vehicleId,
+        responseType: 'JSON'
+      }
+    };
+
+    return options;
+  },
+  getSecurityStatusService: vehicleId => {
+    let options = {
+      method: 'POST',
+      url: 'http://gmapi.azurewebsites.net/getSecurityStatusService',
+      headers: {
+        'content-type': 'application/json'
+      },
+      data: {
+        id: vehicleId,
+        responseType: 'JSON'
+      }
+    };
+
+    return options;
+  },
+  getEnergyService: vehicleId => {
+    let options = {
+      method: 'POST',
+      url: 'http://gmapi.azurewebsites.net/getEnergyService',
+      headers: {
+        'content-type': 'application/json'
+      },
+      data: {
+        id: vehicleId,
+        responseType: 'JSON'
+      }
+    };
+
+    return options;
+  },
+  actionEngineService: (vehicleId, action) => {
+    let options = {
+      method: 'POST',
+      url: 'http://gmapi.azurewebsites.net/actionEngineService',
+      headers: {
+        'content-type': 'application/json'
+      },
+      data: {
+        id: vehicleId,
+        command: formatEngineActionPayload(action),
+        responseType: 'JSON'
+      }
+    };
+    return options;
+  }
 };
