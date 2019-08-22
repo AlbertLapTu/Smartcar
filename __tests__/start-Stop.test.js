03981a5c78545322a80358a1d4c9b779c4f2ef69
Your Name <itzalbert2@gmail.com>
2019-08-21 21:38:16 -0700

    Add start-Stop tests

---
+++ __tests__/start-Stop.test.js
@@ -0,0 +1,41 @@
+const postMock = require('../__mocks__/POST/start-Stop/start-Stop.mocks');
+const invalidMock = require('../__mocks__/utils/globalMocks');
+
+describe('POST request to Lock/Unlock GM vehicle', () => {
+  let startCarResponse = null;
+  let stopCarResponse = null;
+  let invalidVehicle = null;
+
+  beforeAll(() => {
+    postMock
+      .startCar()
+      .then(res => {
+        startCarResponse = res;
+      })
+      .catch(err => {
+        throw new Error('Unable to start car');
+      });
+
+    postMock
+      .stopCar()
+      .then(res => {
+        stopCarResponse = res;
+      })
+      .catch(err => {
+        throw new Error('Unable to stop car');
+      });
+
+    invalidMock
+      .getInvalidVehicle(1555)
+      .then(res => {
+        invalidVehicle = res;
+      })
+      .catch(err => {
+        throw new Error('Unabel to receive invalid vehicle data');
+      });
+  });
+
+  test('Car should be in a stopped state when turning on, then off', () => {
+    const mockStartCar = jest.fn();
+  });
+});
