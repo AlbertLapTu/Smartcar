4782ec6aad6429006ae856a5368b17a0e28915ce
Your Name <itzalbert2@gmail.com>
2019-08-23 10:09:31 -0700

    Add invalid id error handling

--- server/api/vehicles/vehicles.routes.js
+++ server/api/vehicles/vehicles.routes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const { handleInvalidId } = require('../../lib/errorHandlers');
 const {
   getVehicleInfoFromGm,
   getVehicleDoorInfo,
@@ -8,29 +9,63 @@ const {
   startOrStopEngine
 } = require('./vehicles.controllers');
 
+/*
+
+TODO: 
+
+[x] - Handle errors if passed an invalid parameter
+[] - Handle error if querying for an invalid response object?
+
+*/
+
 router.get('/:id', (req, res, next) => {
   const { id } = req.params;
+  if (!id || !handleInvalidId(id)) {
+    const error = new Error('Not a valid id, ID parameter expects numbers as a valid ID type');
+    error.httpStatusCode = 400;
+    return next(error);
+  }
   getVehicleInfoFromGm(req, res, next, id);
 });
 
 router.get('/:id/doors', (req, res, next) => {
   const { id } = req.params;
+  if (!id || !handleInvalidId(id)) {
+    const error = new Error('Not a valid id, ID parameter expects numbers as a valid ID type');
+    error.httpStatusCode = 400;
+    return next(error);
+  }
   getVehicleDoorInfo(req, res, next, id);
 });
 
 router.get('/:id/fuel', (req, res, next) => {
   const { id } = req.params;
+  if (!id || !handleInvalidId(id)) {
+    const error = new Error('Not a valid id, ID parameter expects numbers as a valid ID type');
+    error.httpStatusCode = 400;
+    return next(error);
+  }
   getFuelRange(req, res, next, id);
 });
 
 router.get('/:id/battery', (req, res, next) => {
   const { id } = req.params;
+  if (!id || !handleInvalidId(id)) {
+    const error = new Error('Not a valid id, ID parameter expects numbers as a valid ID type');
+    error.httpStatusCode = 400;
+    return next(error);
+  }
   getBatteryRange(req, res, next, id);
 });
 
 router.post('/:id/engine', (req, res, next) => {
   const { id } = req.params;
   const { action } = req.body;
+  if (!id || !action || !handleInvalidId(id)) {
+    const error = new Error('A valid id or action_type is required');
+    error.httpStatusCode = 400;
+    return next(error);
+  }
   startOrStopEngine(req, res, next, id, action);
 });
 
