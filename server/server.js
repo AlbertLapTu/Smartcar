4aa548349108cb9fb48f8ebaf93c666f2216c71e
Your Name <itzalbert2@gmail.com>
2019-08-23 10:59:33 -0700

    Small UI layer

--- server/server.js
+++ server/server.js
@@ -5,6 +5,20 @@ require('./middleware/appMiddleware')(app);
 
 app.use('/vehicles/', vehicleRoutes);
 
+app.get('/', (req, res, next) => {
+  res.send(`
+    <h1>Welcome to the smaller version of Smartcar!<h1>
+    <h3>List of available endpoints
+    <ul>
+      <li>GET /vehicles/:id</li>
+      <li>GET /vehicles/:id/doors</li>
+      <li>GET /vehicles/:id/(fuel || battery)</li>
+      <li>POST /vehicles/:id/engine</li>
+    </ul>
+  `);
+});
+
+//Global error handler
 app.use((err, req, res, next) => {
   res.status(404).json({
     error: 'Invalid request. Check console messages for additional error detail',
